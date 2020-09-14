59b902803f624ebf54e546faf000f6ae463bbf7f
Spriha <sprihapradhan2@gmail.com>
2020-09-14 17:23:54 -0500

    new file

---
+++ TestR.R
@@ -0,0 +1,11 @@
+#Chapter 1 - Introduction
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Explore the mtcars data frame with str()
+str(mtcars)
+
+# Execute the following command - cyl is treated as continuos
+ggplot(mtcars, aes(cyl, mpg)) +
+  geom_point()
